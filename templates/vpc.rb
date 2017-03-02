3d60ec0b966f67bdba9a965d18ac2b51771f2b23
Vivek Chaudhari <vivek_chaudhari@rapid7.com>
2017-03-02 11:20:11 -0500

    Add VPC

--- templates/vpc.rb
+++ templates/vpc.rb
@@ -4,5 +4,9 @@ module Templates
   VPC = Convection.template do
     description 'VPC with Public and Private Subnets (NAT)'
 
+    ec2_vpc 'DemoVPC' do
+      network '10.10.10.0/16'
+      tag 'Name', "#{stack.cloud}-#{stack.name}"
+    end
   end
 end
